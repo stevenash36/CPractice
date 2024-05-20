073872fdb0519af391b5f98cc1852baf542bbfa3
Hermit36 <s0054003@gmail.com>
2024-05-20 14:58:17 +0800

    費氏數列

---
+++ 費氏數列.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[20];
+            a[0] = 1;
+            a[1] = 1;
+            for (int i = 2; i < a.Length; i++)
+            {
+                a[i] = a[i-1] + a[i-2];
+                Console.WriteLine(a[i]);
+            }
+        }
+    }
+
+
+}
